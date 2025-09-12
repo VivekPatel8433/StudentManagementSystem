a104b7bb3994df6584d3e153c3b2b46b9d07c8ef
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-09-11 20:48:26 -0600

    Add project files.

---
+++ Student Management System/Student Management System/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Student_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
