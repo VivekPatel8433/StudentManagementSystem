57d779b82b25292147ea336d16309eebd4395a9f
Vivekbhai Patel <VivekPatel8433@gmail.com>
2025-09-13 20:51:57 -0600

    Classes Creation!

--- Student Management System/Student Management System/Program.cs
+++ Student Management System/Student Management System/Program.cs
@@ -6,35 +6,62 @@ namespace StudentManagementSystem
     {
         string MarkConvertor(int mark);
     }
-    class Student
+
+    class Person // Common
     {
         public string Name { get; set; }
-        public int Id { get; set; }
-        public double Marks { get; set; }
+        public int Id { get; set; } 
 
-        public Student(string name, int id, double marks)
+        public Person (string name, int id)
         {
             Name = name;
             Id = id;
-            Marks = marks;
+        }
+
+    }
+
+
+    class Student : Person // Student is a person 
+    {
+        public int Marks { get; set; }
+        public Student(string Name, int Id, int marks) : base(Name, Id) 
+        {
+            Marks = Marks;
+        }      
+    }
+
+    class Instructors: Person // Instructors are person
+    {
+        public string CoursesTaught{ get; set; }
+
+        public Instructors(string Name, int Id, string coursestaught): base(Name, Id)
+        {
+            CoursesTaught = coursestaught;
         }
     }
 
+    class Staff
+    {
+
+    }
     class MarkConvert : IMarkConversion
     {
         public virtual string MarkConvertor(int marks)
         {
+
             if (marks >= 90) return "A";
             if (marks >= 75) return "B";
             if (marks >= 60) return "C";
             if (marks >= 50) return "D";
             return "F";
+
         }
     }
 
+
     internal class StudentsDetails
     {
-        private Student[] students = new Student[10];
+        private Student[] students = new Student[10]; // Maximum of 10 Students Stored
         private int initialStudents = 0;
 
         public void AddStudent(string name, int id, int mark)
@@ -53,7 +80,7 @@ class Program
             Console.WriteLine("--Welcome To Student Management System!--");
 
             Console.Write("Enter Your Name: ");
-            string name = Console.ReadLine();
+            string name = Console.ReadLine().Trim();
 
             Console.Write("Enter Your Id: ");
             int id = Convert.ToInt32(Console.ReadLine());
@@ -61,17 +88,19 @@ class Program
             Console.Write("Enter Your Marks: ");
             int marks = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("--Student Grade--");
+            
 
-            Console.WriteLine("Grade Equivalent: ");
 
             StudentsDetails studentdetails =  new StudentsDetails();
-            MarkConvert markconvertor = new MarkConvert();      
-            
+            MarkConvert markconvertor = new MarkConvert();
+
+            Console.WriteLine("--Student Grade--");
+            Console.WriteLine(markconvertor.MarkConvertor(marks));
+
 
             studentdetails.AddStudent(name, id , marks);
-
             
+
         }
     }
 }
